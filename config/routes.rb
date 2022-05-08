7d2d9d4a1af801cf9fd09d46e420cf6245d78b58
limagisele <limagisele05@gmail.com>
2022-05-08 14:45:53 +1000

    devise and user set up

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
+  
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
