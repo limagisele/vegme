0b9d3ed25457bd4747b4b7fca1f0efc103e9df88
limagisele <limagisele05@gmail.com>
2022-05-09 18:31:47 +1000

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Address.destroy_all
+MenuItem.destroy_all
+User.destroy_all
+
+params = { customer: { email: 'c@test.com', password: '123456', full_name: 'Anna', phone: '04000400', address_attributes: { street: '3 Station St', city: 'Melbourne', postcode: 3001 } } }
+customer = User.create(params[:customer])
+customer.add_role :customer
+
+params1 = { restaurant1: { email: 'r1@test.com', password: '123456', full_name: 'VeBurgers', phone: '04110411', address_attributes: { street: '120 King St', city: 'Melbourne', postcode: 3000 } } }
+restaurant1 = User.create(params1[:restaurant1])
+restaurant1.add_role :restaurant
+
+params2 = { restaurant2: { email: 'r2@test.com', password: '123456', full_name: 'Pizza Vegs', phone: '04220422', address_attributes: { street: '67 Elizabeth St', city: 'Melbourne', postcode: 3002 } } }
+restaurant2 = User.create(params2[:restaurant2])
+restaurant2.add_role :restaurant
+
+puts "Users: #{User.count}"
+puts "Roles: #{Role.count}"
+puts "Adresses: #{Address.count}"
