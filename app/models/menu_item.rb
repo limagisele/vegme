3891669317f52571a92b7e97a18414d44b82ef42
limagisele <limagisele05@gmail.com>
2022-05-15 19:08:10 +1000

    add dependent: :destroy

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,7 +1,7 @@
 class MenuItem < ApplicationRecord
     belongs_to :user
-    has_one_attached :photo
-    has_many :order_menu_items
-    has_many :orders, through: :order_menu_items
+    has_one_attached :photo, dependent: :destroy
+    has_many :order_menu_items, dependent: :destroy
+    has_many :orders, through: :order_menu_items, dependent: :destroy
 
 end
