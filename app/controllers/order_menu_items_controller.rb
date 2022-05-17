1eeb752543e4a4367d03e492c7e5c4878df2e16a
limagisele <limagisele05@gmail.com>
2022-05-17 19:20:57 +1000

    adjust layout for small screen

--- app/controllers/order_menu_items_controller.rb
+++ app/controllers/order_menu_items_controller.rb
@@ -6,7 +6,7 @@ class OrderMenuItemsController < ApplicationController
 
   def index
     @order_items = @order.order_menu_items
-    if user_signed_in? && current_user.has_role?(:customer) && @order_items.size != 0
+    if user_signed_in? && current_user.has_role?(:customer) && !@order_items.empty?
       session = Stripe::Checkout::Session.create(
         payment_method_types: ['card'],
         customer_email: current_user.email,
