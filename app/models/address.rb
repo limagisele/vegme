5d2d88c79548d9ba5a3cdd9b9e79c91d3eb96f4d
limagisele <limagisele05@gmail.com>
2022-05-18 22:13:06 +1000

    add input validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :street, :city, presence: true
+  validates :postcode, numericality: { only_integer: true }, length: { is: 4 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
+  validates :full_name, presence: true, length: { in: 3..30 }
+  validates :phone, length: { in: 10..12 }, numericality: { only_integer: true }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   def with_address
     build_address if address.nil?
     self
   end
+
 end
