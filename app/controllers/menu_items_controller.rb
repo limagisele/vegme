2c243214d888ca937099df7aee143672c04ec988
limagisele <limagisele05@gmail.com>
2022-05-19 17:24:57 +1000

    add #includes for eager loading

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -8,23 +8,24 @@ class MenuItemsController < ApplicationController
 
   def index
     if user_signed_in? && current_user.has_role?(:restaurant)
-      # When the users is a restaurant it will see only its own menu
       # Querying user only with required fields for the view (full_name and id)
-      @restaurants = Role.find_by_name('restaurant').users.where(id: current_user.id).select(:full_name, :id)
+      # Eager loading used to query Users and MenuItems all at once
+
+      # When the user is a restaurant, only its restaurant will be displayed
+      @restaurants = User.select(:full_name, :id).includes(:menu_items).where(id: current_user.id)
     else
       # When user is a customer, all restaurants will be displayed
-      # Querying user only with required fields for the view (full_name and id)
-      @restaurants = Role.find_by_name('restaurant').users.select(:full_name, :id)
+      @restaurants = Role.find_by_name('restaurant').users.select(:full_name, :id).includes(:menu_items)
     end
   end
 
   def show_menu
     # Finding the requested restaurant
     # Querying only the attributes required in the view file
-    @restaurant = User.select(:full_name, :id).find(params[:id])
-    # Defining a variable to list all menu_items from the chosen restaurant
-    # Querying only the attributes required in the view file
-    @menu = @restaurant.menu_items.select(:name, :price, :id)
+    # Using eager loading to query User, MenuItems and Attachment at once to minimise db calls in the view loop
+    @restaurant = User.select(:full_name, :id).includes(menu_items: {photo_attachment: [:blob] }).find(params[:id])
+    # Defining an array with all menu_items associated with the chosen restaurant
+    @menu = @restaurant.menu_items
   end
 
   def show
@@ -83,8 +84,8 @@ class MenuItemsController < ApplicationController
     return params.require(:menu_item).permit(:name, :description, :price, :available, :user_id, :photo)
   end
 
-  # Creating a variable with a specific menu_item to be displayed and updated
-  # Selecting only the attributes needed in all views
+  # Creating a variable with a specific menu_item to be displayed, updated and deleted
+  # Selecting only the attributes needed in views and actions
   def set_menu_item
     @menu_item = MenuItem.select(:id, :user_id, :name, :description, :price, :available).find(params[:id])
   end
