73783a577176172f425877cc66ab0453f728279b
limagisele <limagisele05@gmail.com>
2022-05-20 10:36:59 +1000

    fix bug on 'available' attribute validation

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -6,7 +6,6 @@ class MenuItem < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 30, maximum: 400 }
     validates :price, presence: true, numericality: { in: 1..99 }
-    validates :available, presence: true
     after_commit :add_default_photo, on: [:create, :update]
 
     private
